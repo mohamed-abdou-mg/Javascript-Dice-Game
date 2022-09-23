881bab508b2432de31ae7fc3ec41e0d9bba056d2
mohamed.abdou.mg <mohamed.abdou.mg@gmail.com>
2022-09-23 19:17:35 +0200

    display winner player and activate hold button

--- script.js
+++ script.js
@@ -26,15 +26,40 @@ document.querySelector('.btn--roll').addEventListener('click', function(){
         document.getElementById('current--' + activePlayer).textContent = roundScore;
     }else{
         //Next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-
-        roundScore = 0;
-        document.querySelector('.dice').style.display = 'none';
-        document.getElementById('current--0').textContent = '0';
-        document.getElementById('current--1').textContent = '0';
-
-        document.querySelector('.player--0').classList.toggle('player--active');
-        document.querySelector('.player--1').classList.toggle('player--active');
+        nextPlayer();
     }
-
 });
+
+document.querySelector('.btn--hold').addEventListener('click', function(){
+    //update global score for the player
+    scores[activePlayer] += roundScore;
+
+    //update ui
+    document.getElementById('score--' + activePlayer).textContent = scores[activePlayer];
+
+    //check the winner
+    if(scores[activePlayer] >= 10){
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.btn--hold').style.display = 'none';
+        document.querySelector('.btn--roll').style.display = 'none';
+        document.querySelector('.player--' + activePlayer).classList.add('player--winner');
+        document.querySelector('.player--' + activePlayer).classList.remove('active');
+        document.getElementById('name--' + activePlayer).textContent = 'Winner!';
+    }else{
+        //Next player
+        nextPlayer();
+    }
+});
+
+
+function nextPlayer(){
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+    roundScore = 0;
+    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('current--0').textContent = '0';
+    document.getElementById('current--1').textContent = '0';
+
+    document.querySelector('.player--0').classList.toggle('player--active');
+    document.querySelector('.player--1').classList.toggle('player--active');
+}
