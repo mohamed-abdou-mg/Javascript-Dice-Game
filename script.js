01a1d50f0f4e68b7614b8b93811ab49d25cb8055
mohamed.abdou.mg <mohamed.abdou.mg@gmail.com>
2022-09-23 18:56:55 +0200

    update round score for each player

--- script.js
+++ script.js
@@ -12,11 +12,29 @@ document.getElementById('current--1').textContent = '0';
 document.querySelector('.btn--roll').addEventListener('click', function(){
 
     // generate random number
-    var random = Math.floor(Math.random() * 6) + 1;
+    var dice = Math.floor(Math.random() * 6) + 1;
 
     //display dice
     var diceDom = document.querySelector('.dice');
     diceDom.style.display = 'block';
-    diceDom.src = 'dice-' + random + '.png';
+    diceDom.src = 'dice-' + dice + '.png';
+
+    //update round score
+    if(dice !== 1){
+        //Update score
+        roundScore += dice;
+        document.getElementById('current--' + activePlayer).textContent = roundScore;
+    }else{
+        //Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+        roundScore = 0;
+        document.querySelector('.dice').style.display = 'none';
+        document.getElementById('current--0').textContent = '0';
+        document.getElementById('current--1').textContent = '0';
+
+        document.querySelector('.player--0').classList.toggle('player--active');
+        document.querySelector('.player--1').classList.toggle('player--active');
+    }
 
 });
